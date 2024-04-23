f955552795fabf78403b8f2865977272061bb5f4
Klim Tsoutsman <klim@tsoutsman.com>
2024-04-23 12:46:54 +1000

    Use closure API
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::sync::OnceLock;
 
-pub use jni::{objects::JObject, JavaVM};
+pub use jni::{objects::JObject, JNIEnv, JavaVM};
 
 static VM: OnceLock<JavaVM> = OnceLock::new();
 
@@ -10,22 +10,25 @@ pub fn set_vm(vm: JavaVM) -> Result<(), JavaVM> {
     VM.set(vm)
 }
 
-#[inline]
-pub fn vm() -> Option<&'static JavaVM> {
-    VM.get()
-}
-
-static ACTIVITY_GETTER: OnceLock<fn() -> &'static JObject<'static>> = OnceLock::new();
+// NOTE: These lifetimes are wrong, but that's fine because we don't expose them
+// to the user.
+static ACTIVITY_GETTER: OnceLock<fn() -> (Option<JNIEnv<'static>>, &'static JObject<'static>)> =
+    OnceLock::new();
 
 #[inline]
 #[cfg(feature = "set")]
 pub fn set_current_activity_getter(
-    f: fn() -> &'static JObject<'static>,
-) -> Result<(), fn() -> &'static JObject<'static>> {
+    f: fn() -> (Option<JNIEnv<'static>>, &'static JObject<'static>),
+) -> Result<(), fn() -> (Option<JNIEnv<'static>>, &'static JObject<'static>)> {
     ACTIVITY_GETTER.set(f)
 }
 
-#[inline]
-pub fn current_activity() -> Option<&'static JObject<'static>> {
-    ACTIVITY_GETTER.get().map(|f| f())
+pub fn with_activity<T, R>(f: T) -> Option<R>
+where
+    T: for<'a, 'b, 'c, 'd> Fn(&'a mut JNIEnv<'b>, &'c JObject<'d>) -> R,
+{
+    let getter = ACTIVITY_GETTER.get()?;
+    let (maybe_env, object) = getter();
+    let mut env = maybe_env.or_else(|| VM.get()?.get_env().ok())?;
+    Some(f(&mut env, object))
 }
